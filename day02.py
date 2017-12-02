f6727260a3c853646633c49217a9a74f23f15fab
adeak <adeak@users.noreply.github.com>
2017-12-02 12:16:57 +0100

    Day 2

---
+++ day02.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def day02(inp):
+    '''Compute checksum from file'''
+    # square cases are faster
+    try:
+        return np.loadtxt(inp).ptp(axis=1).sum()
+    except ValueError:
+        # this is probably just a test case
+        return sum(np.fromstring(row,sep=' ').ptp() for row in open(inp).read().rstrip().split('\n'))
+
+def day02b(inp):
+    '''Compute part 2 checksum from filename'''
+    dat = np.loadtxt(inp)
+
+    # divide each pair of elements in each row, set diagonals to 0
+    divs = dat[:,None,:]/dat[:,:,None]
+    ran = range(divs.shape[1])
+    divs[:,ran,ran] = 0
+    inds = divs==divs.astype(int)
+
+    return divs[inds].sum()
